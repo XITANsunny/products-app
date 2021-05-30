16b72862de5a9a754b6f62c22057cac4a46faf05
Xi Tan <xitan@Xis-MacBook-Pro.local>
2021-05-30 15:45:56 +1000

    Update test script

--- src/integration.test.js
+++ src/integration.test.js
@@ -3,7 +3,10 @@ const TIMEOUT = 4000;
 
 // Go to the specified path and wait for the domcontent to load before running the tests
 beforeAll(async () => {
-  await page.goto('http://localhost:3000', {waitUntil: 'domcontentloaded'});
+  await page.goto('http://localhost:5000', {
+    waitUntil: 'domcontentloaded',
+    timeout: 1e4, // The page takes time to build, therefore a long timeout just in case.
+  });
 });
 
 describe('Integration test of the app', () => {
